68b7230bd3f9e000e8f80e9ef9721fa5e5fb76ba
Willy139 <raihanwillynsyh139@gmail.com>
2024-12-04 15:13:50 +0700

    lagi js 2

--- js/script.js
+++ js/script.js
@@ -2,8 +2,24 @@ const btn = document.getElementById('hitung');
 
 btn.addEventListener('click', function(){
 
-    let tinggi = document.querySelector('tinggi-badan-input');
-    let berat = document.querySelector('berat-badan-input');
+    let tinggi = document.querySelector('#tinggi-badan-input').value;
+    let berat = document.querySelector('#berat-badan-input').value;
 
-    if(tinggi)
+    if(tinggi == '' || berat == ''){
+        alert('Tolong isi kolom yang kosong');
+        return;
+    }
+
+    //BMI = berat in KG / (tinggi in M * tinggi in M)
+
+    tinggi = tinggi / 100
+
+    let BMI = (berat / (tinggi * tinggi));
+
+    BMI = BMI.toFixed(2);
+
+    console.log('BMI')
+
+    document.querySelector("#result").innerHTML= BMI;
+
 });
