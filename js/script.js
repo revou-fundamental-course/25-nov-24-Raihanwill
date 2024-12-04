3cc98a0c311b7498184b1e0c9a94d64b93ac2a0c
Willy139 <raihanwillynsyh139@gmail.com>
2024-12-04 13:52:33 +0700

    lagi js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('hitung');
+
+btn.addEventListener('click', function(){
+
+    let tinggi = document.querySelector('tinggi-badan-input');
+    let berat = document.querySelector('berat-badan-input');
+
+    if(tinggi)
+});
